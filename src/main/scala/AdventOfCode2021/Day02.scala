bd360e8b757462182c5ab41195de0bb8b78d0793
maneatingape <44142177+maneatingape@users.noreply.github.com>
2021-12-02 21:16:18 +0000

    Convert Day2 to use case objects instead of vals

--- src/main/scala/AdventOfCode2021/Day02.scala
+++ src/main/scala/AdventOfCode2021/Day02.scala
@@ -1,20 +1,20 @@
 package AdventOfCode2021
 
 object Day02 {
-  case class Command(prefix: String) {
+  sealed abstract class Command(prefix: String) {
     def unapply(s: String): Option[Int] = Option.when(s.startsWith(prefix))(s.drop(prefix.length + 1).toInt)
   }
 
-  private val forward = Command("forward")
-  private val down = Command("down")
-  private val up = Command("up")
+  case object Forward extends Command("forward")
+  case object Down extends Command("down")
+  case object Up extends Command("up")
 
   def part1(input: Seq[String]): Int = {
     val (distance, depth) = input.foldLeft((0, 0)) { case ((distance, depth), command) =>
       command match {
-        case forward(value) => (distance + value, depth)
-        case down(value) => (distance, depth + value)
-        case up(value) => (distance, depth - value)
+        case Forward(value) => (distance + value, depth)
+        case Down(value) => (distance, depth + value)
+        case Up(value) => (distance, depth - value)
       }
     }
     distance * depth
@@ -23,9 +23,9 @@ object Day02 {
   def part2(input: Seq[String]): Int = {
     val (distance, depth, _) = input.foldLeft((0, 0, 0)) { case ((distance, depth, aim), command) =>
       command match {
-        case forward(value) => (distance + value, depth + aim * value, aim)
-        case down(value) => (distance, depth, aim + value)
-        case up(value) => (distance, depth, aim - value)
+        case Forward(value) => (distance + value, depth + aim * value, aim)
+        case Down(value) => (distance, depth, aim + value)
+        case Up(value) => (distance, depth, aim - value)
       }
     }
     distance * depth
