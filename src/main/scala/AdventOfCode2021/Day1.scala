cf1512dfbfc77c3e00af5c48ddc146a3461c0261
maneatingape <44142177+maneatingape@users.noreply.github.com>
2021-12-02 14:17:21 +0000

    Day 1

---
+++ src/main/scala/AdventOfCode2021/Day1.scala
@@ -0,0 +1,18 @@
+package AdventOfCode2021
+
+object Day1 {
+  val sample = Seq(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
+
+  val data = io.Source.fromResource("AdventOfCode2021/Day1.txt").getLines.map(_.toInt).toSeq
+
+  def part1(input: Seq[Int]) = input.sliding(2).count { case Seq(x, y) => y > x }
+
+  def part2(input: Seq[Int]) = part1(input.sliding(3).map(_.sum).toSeq)
+
+  def main(args: Array[String]) = {
+    println(part1(sample))
+    println(part2(sample))
+    println(part1(data))
+    println(part2(data))
+  }
+}
