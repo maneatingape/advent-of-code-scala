83f3ebca9514e3517425a2220051b6c7ecebc55b
maneatingape <44142177+maneatingape@users.noreply.github.com>
2021-12-04 13:55:47 +0000

    Linting

--- src/main/scala/AdventOfCode2021/Day04.scala
+++ src/main/scala/AdventOfCode2021/Day04.scala
@@ -3,29 +3,25 @@ package AdventOfCode2021
 object Day04:
   val indices =
     val rows = Seq.tabulate(5, 5)((row, column) => 5 * row + column)
-    val columns = Seq.tabulate(5, 5)((row, column) => row + 5 * column)
+    val columns = rows.transpose
     rows ++ columns
 
   case class Win(score: Int, turn: Int)
 
-  case class Board(numbers: Seq[Int], score: Option[Win]):
+  case class Board(numbers: Seq[Int], win: Option[Win] = None):
     def mark(number: Int, turn: Int): Board =
       val nextNumbers = numbers.map(n => if n == number then 0 else n)
       val isWin = indices.exists(_.forall(i => nextNumbers(i) == 0))
-      val nextScore = Option.when(isWin)(Win(nextNumbers.sum * number, turn))
-      Board(nextNumbers, score.orElse(nextScore))
+      val nextWin = Option.when(isWin)(Win(nextNumbers.sum * number, turn))
+      Board(nextNumbers, win.orElse(nextWin))
 
   def play(input: Seq[String]): Seq[Win] =
     val numbers = input.head.split(",").map(_.toInt).toSeq
-    val boards = input.tail.grouped(6).toSeq.map { xs =>
-      Board(xs.mkString(" ").split(" ").filter(!_.isEmpty).map(_.toInt).toSeq, None)
-    }
+    val boards = input.tail.grouped(6).toSeq.map(s => Board(s.mkString(" ").split(" ").flatMap(_.toIntOption)))
     numbers
       .zipWithIndex
-      .foldLeft(boards) { case (boards, (number, index)) =>
-        boards.map(_.mark(number, index))
-      }
-      .flatMap(_.score)
+      .foldLeft(boards) { case (boards, (number, turn)) => boards.map(_.mark(number, turn)) }
+      .flatMap(_.win)
 
   def part1(input: Seq[String]): Int = play(input).minBy(_.turn).score
 
