64f66a98dde213909e11048c16d390b94204ccb0
maneatingape <44142177+maneatingape@users.noreply.github.com>
2021-12-04 13:22:15 +0000

    Track winning turn inside Board object

--- src/main/scala/AdventOfCode2021/Day04.scala
+++ src/main/scala/AdventOfCode2021/Day04.scala
@@ -1,6 +1,8 @@
 package AdventOfCode2021
 
 object Day04:
+  case class Win(score: Int, turn: Int)
+
   object Board:
     val indices =
       val rows = Seq.tabulate(5, 5)((row, column) => 5 * row + column)
@@ -9,8 +11,8 @@ object Day04:
 
     def apply(numbers: Seq[Int]) = new Board(numbers, Array.fill(25)(false), None)
 
-  case class Board(numbers: Seq[Int], marked: Array[Boolean], score: Option[Int]):
-    def mark(number: Int): Board =
+  case class Board(numbers: Seq[Int], marked: Array[Boolean], score: Option[Win]):
+    def mark(number: Int, turn: Int): Board =
       var score = this.score
 
       if score.isEmpty then
@@ -19,34 +21,26 @@ object Day04:
           marked(index) = true
           if Board.indices.exists(_.forall(i => marked(i))) then
             val unmarkedSum = numbers.zipWithIndex.map((n,i) => if marked(i) then 0 else n).sum
-            score = Some(unmarkedSum * number)
+            score = Some(Win(unmarkedSum * number, turn))
+
       Board(numbers, marked, score)
     end mark
 
-  def parseNumbersAndBoards(input: Seq[String]): (Seq[Int], Seq[Board]) =
+  def play(input: Seq[String]): Seq[Win] =
     val numbers = input.head.split(",").map(_.toInt).toSeq
     val boards = input.tail.grouped(6).toSeq.map { xs =>
       Board(xs.mkString(" ").split(" ").filter(!_.isEmpty).map(_.toInt).toSeq)
     }
-    (numbers, boards)
-
-  def part1(input: Seq[String]): Int =
-    def firstWinner(numbers: Seq[Int], boards: Seq[Board]): Int = boards
-      .map(_.mark(numbers.head))
-      .find(_.score.isDefined)
+    numbers
+      .zipWithIndex
+      .foldLeft(boards) { case (boards, (number, index)) =>
+        boards.map(_.mark(number, index))
+      }
       .flatMap(_.score)
-      .getOrElse(firstWinner(numbers.tail, boards))
 
-    val (numbers, boards) = parseNumbersAndBoards(input)
-    firstWinner(numbers, boards)
+  def part1(input: Seq[String]): Int = play(input).minBy(_.turn).score
 
-  def part2(input: Seq[String]): Int =
-    def lastWinner(numbers: Seq[Int], boards: Seq[Board]): Int = boards match
-      case Seq(head) => numbers.foldLeft(head)((board, number) => board.mark(number)).score.get
-      case _ => lastWinner(numbers.tail, boards.map(_.mark(numbers.head)).filter(_.score.isEmpty))
-
-    val (numbers, boards) = parseNumbersAndBoards(input)
-    lastWinner(numbers, boards)
+  def part2(input: Seq[String]): Int = play(input).maxBy(_.turn).score
 
   def main(args: Array[String]): Unit =
     val data = io.Source.fromResource("AdventOfCode2021/Day04.txt").getLines().toSeq
