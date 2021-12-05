946b0f223985f1540bddfd4481772c42d0a4c093
maneatingape <44142177+maneatingape@users.noreply.github.com>
2021-12-05 08:34:24 +0000

    Simplify

--- src/main/scala/AdventOfCode2021/Day05.scala
+++ src/main/scala/AdventOfCode2021/Day05.scala
@@ -1,32 +1,22 @@
 package AdventOfCode2021
 
-import java.lang.Math.{abs, max}
-
 object Day05:
-  case class Point(x: Int, y: Int)
-
-  case class Line(p1: Point, p2: Point):
+  case class Line(x1: Int, y1: Int, x2: Int, y2: Int):
     def compare(a: Int, b: Int) = if a == b then 0 else if a < b then 1 else -1
 
-    def isOrthogonal = p1.x == p2.x || p1.y == p2.y
+    def isOrthogonal = x1 == x2 || y1 == y2
 
-    def points: Seq[Point] =
-      val stepX = compare(p1.x, p2.x)
-      val stepY = compare(p1.y, p2.y)
-      val length = max(abs(p1.x - p2.x), abs(p1.y - p2.y))
-      (0 to length).map(i => Point(p1.x + stepX * i, p1.y + stepY * i))
+    def points: Seq[(Int, Int)] =
+      val stepX = compare(x1, x2)
+      val stepY = compare(y1, y2)
+      val length = Math.max(Math.abs(x1 - x2), Math.abs(y1 - y2))
+      (0 to length).map(i => (x1 + stepX * i, y1 + stepY * i))
 
   def parseLines(input: Seq[String]): Seq[Line] = input
     .map(_.split(" -> ").flatMap(_.split(",")).map(_.toInt))
-    .map { case Array(x1, y1, x2, y2) => Line(Point(x1, y1), Point(x2, y2)) }
+    .map { case Array(x1, y1, x2, y2) => Line(x1, y1, x2, y2) }
 
-  def overlap(lines: Seq[Line]): Int = lines
-    .toSet
-    .subsets(2)
-    .map(_.toSeq)
-    .flatMap { case Seq(line1, line2) => line1.points.intersect(line2.points) }
-    .distinct
-    .length
+  def overlap(lines: Seq[Line]): Int = lines.flatMap(_.points).groupBy(identity).count(_._2.length > 1)
 
   def part1(input: Seq[String]): Int = overlap(parseLines(input).filter(_.isOrthogonal))
 
