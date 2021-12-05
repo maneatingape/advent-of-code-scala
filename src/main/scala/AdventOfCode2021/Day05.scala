fd579e8e9e1e6d4a69d3ccfdc20506afcd02be99
maneatingape <44142177+maneatingape@users.noreply.github.com>
2021-12-05 11:54:07 +0000

    Trying out the "zipAll" method to shave off a line

--- src/main/scala/AdventOfCode2021/Day05.scala
+++ src/main/scala/AdventOfCode2021/Day05.scala
@@ -5,10 +5,9 @@ object Day05:
     def isOrthogonal: Boolean = x1 == x2 || y1 == y2
 
     def points: Seq[(Int, Int)] =
-      val stepX = (x2 - x1).sign
-      val stepY = (y2 - y1).sign
-      val length = (x1 - x2).abs.max((y1 - y2).abs)
-      (0 to length).map(i => (x1 + stepX * i, y1 + stepY * i))
+      val xs = x1 to x2 by (if x1 < x2 then 1 else -1)
+      val ys = y1 to y2 by (if y1 < y2 then 1 else -1)
+      xs.zipAll(ys, x1, y1).map(_ -> _)
 
   def parseLines(input: Seq[String]): Seq[Line] = input
     .map(_.split(" -> ").flatMap(_.split(",")).map(_.toInt))
