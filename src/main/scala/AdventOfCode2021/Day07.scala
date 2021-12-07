b74df10ff50b60567d05c718b216571a90de3043
maneatingape <44142177+maneatingape@users.noreply.github.com>
2021-12-07 14:11:20 +0000

    Linting

--- src/main/scala/AdventOfCode2021/Day07.scala
+++ src/main/scala/AdventOfCode2021/Day07.scala
@@ -1,15 +1,15 @@
 package AdventOfCode2021
 
 object Day07:
-  def constantCost(x1: Int, x2: Int): Int = (x1 - x2).abs
+  def linearCost(x1: Int, x2: Int): Int = (x1 - x2).abs
 
-  def increasingCost(x1: Int, x2: Int): Int = (constantCost(x1, x2) * (constantCost(x1, x2) + 1)) / 2
+  def triangleCost(x1: Int, x2: Int): Int = (linearCost(x1, x2) * (linearCost(x1, x2) + 1)) / 2
 
   def lowestCost(input: Seq[Int], cost: (Int, Int) => Int): Int = (input.min to input.max).map(x => input.map(cost(x, _)).sum).min
 
-  def part1(input: Seq[Int]): Int = lowestCost(input, constantCost)
+  def part1(input: Seq[Int]): Int = lowestCost(input, linearCost)
 
-  def part2(input: Seq[Int]): Int = lowestCost(input, increasingCost)
+  def part2(input: Seq[Int]): Int = lowestCost(input, triangleCost)
 
   def main(args: Array[String]): Unit =
     val data = io.Source.fromResource("AdventOfCode2021/Day07.txt").mkString.trim.split(",").map(_.toInt).toSeq
