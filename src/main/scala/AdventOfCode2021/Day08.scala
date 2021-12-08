829ecd8a990f6165a0767c14327bff98debd7d92
maneatingape <44142177+maneatingape@users.noreply.github.com>
2021-12-08 10:17:11 +0000

    Simplify set logic slightly

--- src/main/scala/AdventOfCode2021/Day08.scala
+++ src/main/scala/AdventOfCode2021/Day08.scala
@@ -3,10 +3,9 @@ package AdventOfCode2021
 object Day08:
   case class Entry(patterns: Seq[String], digits: Seq[String])
 
-  // All possible segments
   val segments = Seq('a', 'b', 'c', 'd', 'e', 'f', 'g')
-  // Map sorted non-scrambled segment pattern to digit
-  val segmentsToDigit = Map("abcefg" -> 0, "cf" -> 1, "acdeg" -> 2, "acdfg" -> 3, "bcdf" -> 4, "abdfg" -> 5, "abdefg" -> 6, "acf" -> 7, "abcdefg" -> 8, "abcdfg" -> 9)
+  // Map *sorted* non-scrambled segment pattern to digit
+  val segmentsToDigit = Seq("abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg").zipWithIndex.toMap
 
   def parseEntry(entry: String): Entry =
     val Array(patterns, digits) = entry.split(" \\| ")
@@ -17,28 +16,27 @@ object Day08:
     case _ => false
 
   def unscrambleEntry(entry: Entry): Int =
-    // One is the only pattern 2 characters long and Seven the only pattern 3 characters long
-    def fromKnownLength(x: Int) = entry.patterns.filter(_.length == x).flatten.toSet
-    val setCF = fromKnownLength(2)
-    val setACF = fromKnownLength(3)
+    // Digit:  0 1 2 3 4 5 6 7 8 9
+    // Length: 6 2 5 5 4 5 6 3 7 6
+    def fromLength(x: Int) = entry.patterns.filter(_.length == x).flatten.toSet
+    // Segment:     a b c d e f g
+    // Occurrences: 8 6 8 7 4 9 7
+    val occurrences = entry.patterns.flatten.groupMapReduce(identity)(_ => 1)(_ + _)
+    def fromOccurrences(x: Int) = segments.filter(occurrences(_) == x).toSet
 
-    // Considering only the digits 0, 2, 3, 5, 6 and 9 then segments a and g occur 6 times, segments d and f 5 times,
-    // segments b and c 4 times and segment e 3 times.
-    val unknownPatterns = entry.patterns.filterNot(isKnownDigit)
-    val unknownOccurrences = unknownPatterns.flatten.groupMapReduce(identity)(_ => 1)(_ + _)
-    def fromUnknownOccurrences(x: Int) = segments.filter(unknownOccurrences(_) == x).toSet
+    val setCF = fromLength(2)
+    val setBCDF = fromLength(4)
 
-    val setE = fromUnknownOccurrences(3)
-    val setBC = fromUnknownOccurrences(4)
-    val setAG = fromUnknownOccurrences(6)
-    val setDF = fromUnknownOccurrences(5)
+    val setE = fromOccurrences(4)
+    val setB = fromOccurrences(6)
+    val setDG = fromOccurrences(7)
+    val setAC = fromOccurrences(8)
+    val setF = fromOccurrences(9)
 
-    val setA = setACF.diff(setCF)
-    val setB = setBC.diff(setCF)
-    val setC = setBC.diff(setB)
-    val setD = setDF.diff(setCF)
-    val setF = setDF.diff(setD)
-    val setG = setAG.diff(setA)
+    val setC = setCF.diff(setF)
+    val setA = setAC.diff(setC)
+    val setG = setDG.diff(setBCDF)
+    val setD = setDG.diff(setG)
 
     val unscramble = Seq(setA, setB, setC, setD, setE, setF, setG).zip(segments).map((k, v) => (k.head, v)).toMap
 
