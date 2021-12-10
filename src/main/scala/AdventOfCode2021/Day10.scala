57f6c5276f975a366c8fc3f8dff20a34a5fa606d
maneatingape <44142177+maneatingape@users.noreply.github.com>
2021-12-10 06:23:28 +0000

    Tidy

--- src/main/scala/AdventOfCode2021/Day10.scala
+++ src/main/scala/AdventOfCode2021/Day10.scala
@@ -1,57 +1,27 @@
 package AdventOfCode2021
 
 object Day10:
+  val opening = Map('(' -> 1, '[' -> 2, '{' -> 3, '<' -> 4)
+  val closing = Map('x' -> 0, ')' -> 3, ']' -> 57, '}' -> 1197, '>' -> 25137)
+  val matching = Map(')' -> '(', ']' -> '[', '}' -> '{', '>' -> '<')
 
-
-  def part1(input: Seq[String]): Int =
-    val results = input.map { line =>
-      val (stack, state) = line.foldLeft((List('x'), 'x')) { case ((stack, state), next) =>
-        if state != 'x' then
-          (stack, state)
-        else next match
-          case '(' | '[' | '{' | '<' => (next :: stack, state)
-          case ')' => if stack.head == '(' then (stack.tail, state) else (stack, next)
-          case ']' => if stack.head == '[' then (stack.tail, state) else (stack, next)
-          case '}' => if stack.head == '{' then (stack.tail, state) else (stack, next)
-          case '>' => if stack.head == '<' then (stack.tail, state) else (stack, next)
-      }
-
-      state match
-        case 'x' => 0
-        case ')' => 3
-        case ']' => 57
-        case '}' => 1197
-        case '>' => 25137
+  def check(line: String): (List[Char], Char ) =
+    line.foldLeft((List('x'), 'x')) { case ((stack, state), next) =>
+      if state != 'x' then (stack, state)
+      else if opening.contains(next) then (next :: stack, state)
+      else if matching(next) == stack.head then (stack.tail, state)
+      else (stack, next)
     }
-    results.sum
 
+  def part1(input: Seq[String]): Long = input.map(check).map(_._2).map(closing).sum
 
   def part2(input: Seq[String]): Long =
     val results = input.map { line =>
-      val (stack, state) = line.foldLeft((List('x'), 'x')) { case ((stack, state), next) =>
-        if state != 'x' then
-          (stack, state)
-        else next match
-          case '(' | '[' | '{' | '<' => (next :: stack, state)
-          case ')' => if stack.head == '(' then (stack.tail, state) else (stack, next)
-          case ']' => if stack.head == '[' then (stack.tail, state) else (stack, next)
-          case '}' => if stack.head == '{' then (stack.tail, state) else (stack, next)
-          case '>' => if stack.head == '<' then (stack.tail, state) else (stack, next)
-      }
-
-      if state == 'x' then
-        stack.dropRight(1).foldLeft(0L) { case (total, next) =>
-          val score = next match
-            case '(' => 1L
-            case '[' => 2L
-            case '{' => 3L
-            case '<' => 4L
-
-          5L * total + score
-        }
-      else -1
+      val (stack, state) = check(line)
+      if state != 'x' then 0L
+      else stack.dropRight(1).foldLeft(0L)((total, next) => 5 * total + opening(next))
     }
-    val sorted = results.filterNot(_ == -1).sorted
+    val sorted = results.filter(_ > 0).sorted
     sorted(sorted.length / 2)
 
   def main(args: Array[String]): Unit =
--- src/test/scala/AdventOfCode2021/Day10Suite.scala
+++ src/test/scala/AdventOfCode2021/Day10Suite.scala
@@ -16,9 +16,9 @@ class Day10Suite extends AnyFunSuite:
     "<{([{{}}[<[[[<>{}]]]>[]]")
 
   test("Part 1 should handle sample input correctly") {
-    assert(Day10.part1(sample) == 26397)
+    assert(Day10.part1(sample) == 26397L)
   }
 
   test("Part 2 should handle sample input correctly") {
-    assert(Day10.part2(sample) == 288957)
+    assert(Day10.part2(sample) == 288957L)
   }
