7b5fe7cc5e784fab1c2211c2ab1030e00119988a
maneatingape <44142177+maneatingape@users.noreply.github.com>
2021-12-11 17:49:02 +0000

    Experiment with extension methods

--- src/main/scala/AdventOfCode2021/Day11.scala
+++ src/main/scala/AdventOfCode2021/Day11.scala
@@ -6,25 +6,22 @@ object Day11:
   case class Point(x: Int, y: Int):
     def updated(dx: Int, dy: Int): Point = Point(x + dx, y + dy)
 
-  case class Grid(energy: Map[Point, Int]):
+  type Grid = Map[Point, Int]
+  extension (energy: Grid)
     def neighbours(point: Point): Seq[Point] = directions.map(point.updated).filter(energy.contains)
-    def incremented(point: Point): Grid = Grid(energy.updated(point, energy(point) + 1))
-    def zeroed(point: Point): Grid = Grid(energy.updated(point, 0))
+    def flashes: Int = energy.values.count(_ == 0)
 
-  case class State(grid: Grid, flashes: Int)
+  def parse(input: Seq[String]): Grid =
+    Seq.tabulate(input.head.size, input.size)((x, y) => Point(x, y) -> input(y)(x).asDigit).flatten.toMap
 
-  def parse(input: Seq[String]): State =
-    val points = Seq.tabulate(input.head.size, input.size)((x, y) => Point(x, y) -> input(y)(x).asDigit)
-    State(Grid(points.flatten.toMap), 0)
-
-  def step(current: State): State =
-    def helper(grid: Grid, todo: Seq[Point], flashed: Set[Point]): State = todo match
-      case Nil => State(grid, flashed.size)
+  def step(current: Grid): Grid =
+    def helper(grid: Grid, todo: Seq[Point], flashed: Set[Point]): Grid = todo match
+      case Nil => grid
       case (head :: tail) =>
         if flashed.contains(head) then helper(grid, tail, flashed)
-        else if grid.energy(head) < 9 then helper(grid.incremented(head), tail, flashed)
-        else helper(grid.zeroed(head), tail ++ grid.neighbours(head), flashed + head)
-    helper(current.grid, current.grid.energy.keys.toSeq, Set())
+        else if grid(head) < 9 then helper(grid.updated(head, grid(head) + 1), tail, flashed)
+        else helper(grid.updated(head, 0), tail ++ grid.neighbours(head), flashed + head)
+    helper(current, current.keys.toSeq, Set())
 
   def part1(input: Seq[String]): Int = Iterator.iterate(parse(input))(step).take(101).map(_.flashes).sum
 
