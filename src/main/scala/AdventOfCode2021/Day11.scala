5df58cbf1f790bd4ad99d4cb40b021891d926386
maneatingape <44142177+maneatingape@users.noreply.github.com>
2021-12-11 07:25:57 +0000

    Tidy

--- src/main/scala/AdventOfCode2021/Day11.scala
+++ src/main/scala/AdventOfCode2021/Day11.scala
@@ -1,58 +1,49 @@
 package AdventOfCode2021
 
+import scala.annotation.tailrec
+
 object Day11:
   val directions = Seq((-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1))
 
-  case class Grid(width: Int, height: Int, energy: Map[Point, Int]):
-    def withinBounds(point: Point): Boolean = point.x >= 0 && point.x < width && point.y >= 0 && point.y < height
-    def neighbours(point: Point): Seq[Point] = directions.map((dx, dy) => Point(point.x + dx, point.y + dy)).filter(withinBounds)
-    def updated(point: Point, value: Int) = copy(energy = energy + (point -> value))
+  case class Point(x: Int, y: Int):
+    def updated(dx: Int, dy: Int): Point = Point(x + dx, y + dy)
 
-  case class Point(x: Int, y: Int)
+  case class Grid(energy: Map[Point, Int]):
+    def points: Seq[Point] = energy.keys.toSeq
+    def neighbours(point: Point): Seq[Point] = directions.map(point.updated).filter(energy.contains)
+    def incremented(point: Point) = copy(energy = energy + (point -> (energy(point) + 1)))
+    def zeroed(point: Point) = copy(energy = energy + (point -> 0))
 
   def parseGrid(input: Seq[String]): Grid =
     val energy = input.map(_.map(_.asDigit))
     val points = Seq.tabulate(energy.head.size, energy.size)((x, y) => Point(x, y) -> energy(y)(x))
-    Grid(energy.head.size, energy.size, points.flatten.toMap)
+    Grid(points.flatten.toMap)
 
-  def step(grid: Grid): (Grid, Int) =
-    def helper(grid: Grid, todo: Seq[Point] = grid.energy.keys.toSeq, flashed: Set[Point] = Set()): (Grid, Int) =
-      val (nextGrid, nextTodo, nextFlashed) = todo.foldLeft((grid, Seq.empty[Point], flashed)) { case ((grid, todo, flashed), point) =>
-        if flashed.contains(point) then
-          (grid, todo, flashed)
-        else if grid.energy(point) < 9 then
-          (grid.updated(point, grid.energy(point) + 1), todo, flashed)
-        else
-          val nextTodo = todo ++ grid.neighbours(point).filterNot(flashed.contains)
-          (grid.updated(point, 0), nextTodo, flashed + point)
-      }
-      if nextTodo.isEmpty then (nextGrid, nextFlashed.size) else helper(nextGrid, nextTodo, nextFlashed)
-    helper(grid, grid.energy.keys.toSeq, Set())
-  end step
-
-  def printGrid(grid: Grid): Unit =
-    for y <- 0 until grid.height do
-      for x <- 0 until grid.width do
-        print(grid.energy(Point(x, y)))
-      println()
-    println()
+  @tailrec
+  def step(currentGrid: Grid, currentTodo: Seq[Point], currentFlashed: Set[Point]): (Grid, Int) =
+    val initial = (currentGrid, Seq.empty[Point], currentFlashed)
+    val (grid, todo, flashed) = currentTodo.foldLeft(initial) { case ((grid, todo, flashed), point) =>
+      if flashed.contains(point) then (grid, todo, flashed)
+      else if grid.energy(point) < 9 then (grid.incremented(point), todo, flashed)
+      else (grid.zeroed(point), todo ++ grid.neighbours(point).filterNot(flashed.contains), flashed + point)
+    }
+    if todo.isEmpty then (grid, flashed.size) else step(grid, todo, flashed)
 
   def part1(input: Seq[String]): Int =
-    val grid = parseGrid(input)
-    val (_, total) = (1 to 100).foldLeft((grid, 0)) { case ((grid, total), _) =>
-      val (nextGrid, flashes) = step(grid)
-      (nextGrid, total + flashes)
+    @tailrec
+    def helper(grid: Grid, iteration: Int = 1, total: Int = 0): Int = {
+      val (nextGrid, flashes) = step(grid, grid.points, Set())
+      if iteration > 100 then total else helper(nextGrid, iteration + 1, total + flashes)
     }
-    total
+    helper(parseGrid(input))
 
   def part2(input: Seq[String]): Int =
-    var grid = parseGrid(input)
-    def countFlashes: Int = {
-      val (nextGrid, flashes) = step(grid)
-      grid = nextGrid
-      flashes
+    @tailrec
+    def helper(grid: Grid, iteration: Int = 1): Int = {
+      val (nextGrid, flashes) = step(grid, grid.points, Set())
+      if flashes == grid.energy.size then iteration else helper(nextGrid, iteration + 1)
     }
-    1 + Iterator.continually(countFlashes).indexWhere(_ == grid.width * grid.height)
+    helper(parseGrid(input))
 
   def main(args: Array[String]): Unit =
     val data = io.Source.fromResource("AdventOfCode2021/Day11.txt").getLines().toSeq
