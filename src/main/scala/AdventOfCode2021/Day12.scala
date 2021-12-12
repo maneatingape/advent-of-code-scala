d69fd5a5d82419168ab08e61af84fd5a9c132ecb
maneatingape <44142177+maneatingape@users.noreply.github.com>
2021-12-12 07:27:32 +0000

    Linting

--- src/main/scala/AdventOfCode2021/Day12.scala
+++ src/main/scala/AdventOfCode2021/Day12.scala
@@ -3,35 +3,26 @@ package AdventOfCode2021
 object Day12:
   type Cave = Map[String, List[String]]
 
-  def small(name: String): Boolean = name != "start" && name.head.isLower
+  def parse(input: Seq[String]): Cave = input.foldLeft[Cave](Map()) { case (cave, line) =>
+    val Array(start, end) = line.split("-")
+    cave.updated(start, end :: cave.getOrElse(start, Nil)).updated(end, start :: cave.getOrElse(end, Nil))
+  }
 
-  def parse(input: Seq[String]): Cave =
-    def helper(input: Seq[String], map: Cave): Cave = input match
-      case Nil => map
-      case head :: tail =>
-        val Array(start, end) = head.split("-")
-        val nextMap = map.updated(start, end :: map.getOrElse(start, Nil)).updated(end, start :: map.getOrElse(end, Nil))
-        helper(tail, nextMap)
-    helper(input, Map())
+  def descend(cave: Cave)(rule: (String, List[String]) => Boolean): Int =
+    def small(name: String): Boolean = name.head.isLower
+    def helper(current: String, path: List[String]): Int = cave(current).foldLeft(0) { (total, next) =>
+      if next == "end" then total + 1
+      else if next == "start" || (small(next) && rule(next, path.filter(small))) then total
+      else total + helper(next, next :: path)
+    }
+    helper("start", Nil)
 
-  def descend(cave: Cave, rule: (String, List[String]) => Boolean): Int =
-    def helper(path: List[String]): Int =
-      cave(path.head).map { next =>
-        if next == "end" then 1
-        else if next == "start" || (small(next) && rule(next, path.filter(small))) then 0
-        else helper(next :: path)
-      }
-      .sum
-    helper(List("start"))
+  def part1(input: Seq[String]): Int = descend(parse(input))((next, path) => path.contains(next))
 
-  def part1(input: Seq[String]): Int = descend(parse(input), (next, path) => path.contains(next))
-
-  def part2(input: Seq[String]): Int =
-    def rule(next: String, path: List[String]): Boolean =
-      val small = (next :: path)
-      val occurrences = small.groupMapReduce(identity)(_ => 1)(_ + _)
-      occurrences.exists((k, v) => v > 2) || occurrences.count((k, v) => v == 2) > 1
-    descend(parse(input), rule)
+  def part2(input: Seq[String]): Int = descend(parse(input)) { (next, path) =>
+    val occurrences = (next :: path).groupMapReduce(identity)(_ => 1)(_ + _).values.toSeq
+    occurrences.exists(_ > 2) || occurrences.count(_ == 2) > 1
+  }
 
   def main(args: Array[String]): Unit =
     val data = io.Source.fromResource("AdventOfCode2021/Day12.txt").getLines().toSeq
