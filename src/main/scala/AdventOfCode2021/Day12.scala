a46edb570ca436c2bb471c072abbaf08f60d2412
maneatingape <44142177+maneatingape@users.noreply.github.com>
2021-12-12 06:27:49 +0000

    Tidy and switch to immutable approach

--- src/main/scala/AdventOfCode2021/Day12.scala
+++ src/main/scala/AdventOfCode2021/Day12.scala
@@ -1,52 +1,37 @@
 package AdventOfCode2021
 
 object Day12:
-  case class Cave(name: String, small: Boolean, paths: List[String])
+  type Cave = Map[String, List[String]]
 
-  def parse(input: Seq[String]): Map[String, Cave] =
-    def createCave(name: String) = Cave(name, name != "start" && name != "end" && name.head.isLower, Nil)
-    def helper(input: Seq[String], map: Map[String, Cave]): Map[String, Cave] = input match
+  def small(name: String): Boolean = name != "start" && name.head.isLower
+
+  def parse(input: Seq[String]): Cave =
+    def helper(input: Seq[String], map: Cave): Cave = input match
       case Nil => map
       case head :: tail =>
-        val Array(first, second) = head.split("-")
-        val start = map.getOrElse(first, createCave(first))
-        val end = map.getOrElse(second, createCave(second))
-        val nextStart = start.copy(paths = second :: start.paths)
-        val nextEnd = end.copy(paths = first :: end.paths)
-        val nextMap = map.updated(first, nextStart).updated(second, nextEnd)
+        val Array(start, end) = head.split("-")
+        val nextMap = map.updated(start, end :: map.getOrElse(start, Nil)).updated(end, start :: map.getOrElse(end, Nil))
         helper(tail, nextMap)
     helper(input, Map())
 
-  def part1(input: Seq[String]): Int =
-    val caverns = parse(input)
-    val paths = collection.mutable.ListBuffer[List[String]]()
+  def descend(cave: Cave, rule: (String, List[String]) => Boolean): Int =
+    def helper(path: List[String]): Int =
+      cave(path.head).map { next =>
+        if next == "end" then 1
+        else if next == "start" || (small(next) && rule(next, path.filter(small))) then 0
+        else helper(next :: path)
+      }
+      .sum
+    helper(List("start"))
 
-    def descend(path: List[String]): Unit =
-      val (head :: tail) = path
-      if head == "end" then paths += path.reverse
-      else caverns(head).paths.filterNot(next => next == "start" || (caverns(next).small && path.contains(next))).foreach(next => descend(next :: path))
-
-    descend(List("start"))
-    paths.length
-  end part1
+  def part1(input: Seq[String]): Int = descend(parse(input), (next, path) => path.contains(next))
 
   def part2(input: Seq[String]): Int =
-    val caverns = parse(input)
-    val paths = collection.mutable.ListBuffer[List[String]]()
-
-    def rule(path: List[String]): Boolean =
-      val small = path.filter(caverns(_).small)
+    def rule(next: String, path: List[String]): Boolean =
+      val small = (next :: path)
       val occurrences = small.groupMapReduce(identity)(_ => 1)(_ + _)
-      occurrences.forall((k, v) => v <= 2) && occurrences.count((k, v) => v == 2) <= 1
-
-    def descend(path: List[String]): Unit =
-      val (head :: tail) = path
-      if head == "end" then paths += path.reverse
-      else caverns(head).paths.filterNot(next => next == "start" || (caverns(next).small && !rule(next :: path))).foreach(next => descend(next :: path))
-
-    descend(List("start"))
-    paths.length
-  end part2
+      occurrences.exists((k, v) => v > 2) || occurrences.count((k, v) => v == 2) > 1
+    descend(parse(input), rule)
 
   def main(args: Array[String]): Unit =
     val data = io.Source.fromResource("AdventOfCode2021/Day12.txt").getLines().toSeq
