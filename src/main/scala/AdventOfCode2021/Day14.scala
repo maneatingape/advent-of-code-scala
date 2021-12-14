ec0aaaa1d04aca9c7ffb9518af7df13d35e638a7
maneatingape <44142177+maneatingape@users.noreply.github.com>
2021-12-14 10:00:44 +0000

    Linting

--- src/main/scala/AdventOfCode2021/Day14.scala
+++ src/main/scala/AdventOfCode2021/Day14.scala
@@ -9,26 +9,21 @@ object Day14:
     def increase(key: String, amount: Long): Totals = map.updated(key, map.getOrElse(key, 0L) + amount)
 
   def insertion(input: String, steps: Int): Long =
-    val Array(polymer, tail) = input.trim.split("\n\n")
+    val Array(head, tail) = input.trim.split("\n\n")
+    val quantity = head.foldLeft[Totals](Map())((map, element) => map.increase(element.toString, 1))
+    val pairs = head.sliding(2).foldLeft[Totals](Map())((map, pair) => map.increase(pair, 1))
     val rules = tail.split("\n").map(_.trim.split(" -> ")).map(a => a(0) -> a(1)).toMap
-
-    val quantity = polymer.foldLeft[Totals](Map())((map, element) => map.increase(element.toString, 1))
-    val pairs = polymer.sliding(2).foldLeft[Totals](Map())((map, pair) => map.increase(pair, 1))
 
     def step(pairs: Totals, quantity: Totals): (Totals, Totals) =
       pairs.foldLeft[(Totals, Totals)]((Map(), quantity)) { case ((pairs, quantity), (key, value)) =>
-        val first = key.head + rules(key)
-        val second = rules(key) + key.last
-
-        val nextPairs = pairs.increase(first, value).increase(second, value)
+        val nextPairs = pairs.increase(key.head + rules(key), value).increase(rules(key) + key.last, value)
         val nextQuantity = quantity.increase(rules(key), value)
-
         (nextPairs, nextQuantity)
       }
 
     val (_, result) = Iterator.iterate((pairs, quantity))(step).drop(steps).next()
     result.values.max - result.values.min
-
+  end insertion
 
   def part1(input: String): Long = insertion(input, 10)
 
