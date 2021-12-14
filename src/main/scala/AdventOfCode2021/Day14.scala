9278750ca6ecc86f8f0bdd2c45c058ed1c7e9a97
maneatingape <44142177+maneatingape@users.noreply.github.com>
2021-12-14 10:57:33 +0000

    The power of types compels you...

--- src/main/scala/AdventOfCode2021/Day14.scala
+++ src/main/scala/AdventOfCode2021/Day14.scala
@@ -1,22 +1,20 @@
 package AdventOfCode2021
 
-import AdventOfCode2021.Day14.Totals
-
 object Day14:
-  type Totals = Map[String, Long]
+  type Quantity = Map[Char, Long]
+  type Pairs = Map[String, Long]
 
-  extension (map: Totals)
-    def increase(key: String, amount: Long): Totals = map.updated(key, map.getOrElse(key, 0L) + amount)
+  extension [A](map: Map[A, Long])
+    def increase(key: A, amount: Long): Map[A, Long] = map.updated(key, map.getOrElse(key, 0L) + amount)
 
-  def insertion(input: String, steps: Int): Long =
-    val Array(head, tail) = input.trim.split("\n\n")
-    val quantity = head.foldLeft[Totals](Map())((map, element) => map.increase(element.toString, 1))
-    val pairs = head.sliding(2).foldLeft[Totals](Map())((map, pair) => map.increase(pair, 1))
-    val rules = tail.split("\n").map(_.trim.split(" -> ")).map(a => a(0) -> a(1)).toMap
+  def insertion(input: Seq[String], steps: Int): Long =
+    val quantity = input.head.groupMapReduce(identity)(_ => 1L)(_ + _)
+    val pairs = input.head.sliding(2).toSeq.groupMapReduce(identity)(_ => 1L)(_ + _)
+    val rules = input.drop(2).map(_.split(" -> ")).map(a => a(0) -> a(1).head).toMap
 
-    def step(pairs: Totals, quantity: Totals): (Totals, Totals) =
-      pairs.foldLeft[(Totals, Totals)]((Map(), quantity)) { case ((pairs, quantity), (key, value)) =>
-        val nextPairs = pairs.increase(key.head + rules(key), value).increase(rules(key) + key.last, value)
+    def step(pairs: Pairs, quantity: Quantity): (Pairs, Quantity) =
+      pairs.foldLeft[(Pairs, Quantity)]((Map(), quantity)) { case ((pairs, quantity), (key, value)) =>
+        val nextPairs = pairs.increase(key.updated(0, rules(key)), value).increase(key.updated(1, rules(key)), value)
         val nextQuantity = quantity.increase(rules(key), value)
         (nextPairs, nextQuantity)
       }
@@ -25,11 +23,11 @@ object Day14:
     result.values.max - result.values.min
   end insertion
 
-  def part1(input: String): Long = insertion(input, 10)
+  def part1(input: Seq[String]): Long = insertion(input, 10)
 
-  def part2(input: String): Long = insertion(input, 40)
+  def part2(input: Seq[String]): Long = insertion(input, 40)
 
   def main(args: Array[String]): Unit =
-    val data = io.Source.fromResource("AdventOfCode2021/Day14.txt").mkString
+    val data = io.Source.fromResource("AdventOfCode2021/Day14.txt").getLines().toSeq
     println(part1(data))
     println(part2(data))
--- src/test/scala/AdventOfCode2021/Day14Suite.scala
+++ src/test/scala/AdventOfCode2021/Day14Suite.scala
@@ -3,26 +3,25 @@ package AdventOfCode2021
 import org.scalatest.funsuite.AnyFunSuite
 
 class Day14Suite extends AnyFunSuite:
-  val sample = """
-    NNCB
-
-    CH -> B
-    HH -> N
-    CB -> H
-    NH -> C
-    HB -> C
-    HC -> B
-    HN -> C
-    NN -> C
-    BH -> H
-    NC -> B
-    NB -> B
-    BN -> B
-    BB -> N
-    BC -> B
-    CC -> N
-    CN -> C
-  """
+  val sample = Seq(
+    "NNCB",
+    "",
+    "CH -> B",
+    "HH -> N",
+    "CB -> H",
+    "NH -> C",
+    "HB -> C",
+    "HC -> B",
+    "HN -> C",
+    "NN -> C",
+    "BH -> H",
+    "NC -> B",
+    "NB -> B",
+    "BN -> B",
+    "BB -> N",
+    "BC -> B",
+    "CC -> N",
+    "CN -> C")
 
   test("Part 1 should handle sample input correctly") {
     assert(Day14.part1(sample) == 1588L)
