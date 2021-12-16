da05a2d3f05f4c540541d4cbf7bb8365f31c834b
maneatingape <44142177+maneatingape@users.noreply.github.com>
2021-12-16 10:39:41 +0000

    Cleanup pattern matching and remove redundant field

--- src/main/scala/AdventOfCode2021/Day16.scala
+++ src/main/scala/AdventOfCode2021/Day16.scala
@@ -10,26 +10,26 @@ object Day16:
   def binary(digits: Seq[Int]): Long = java.lang.Long.parseLong(digits.mkString, 2)
 
   sealed trait Packet
-  case class Literal(version: Long, typeId: Long, value: Long) extends Packet
+  case class Literal(version: Long, value: Long) extends Packet
   case class Operator(version: Long, typeId: Long, packets: List[Packet]) extends Packet
 
   def decode(input: Seq[Int]): (Int, Packet) =
     val version = binary(input.take(3))
     val typeId = binary(input.slice(3, 6))
     val lengthTypeId = input(6)
-    if typeId == 4 then decodeLiteral(version, typeId, input)
+    if typeId == 4 then decodeLiteral(version, input)
     else if lengthTypeId == 1 then decodeOperatorBitLength(version, typeId, input)
     else decodeOperatorSubPackets(version, typeId, input)
 
-  def decodeLiteral(version: Long, typeId: Long, input: Seq[Int]): (Int, Packet) =
+  def decodeLiteral(version: Long, input: Seq[Int]): (Int, Packet) =
     val (_, chunks, read) = Iterator.iterate((1, Seq.empty[Int], 6))(decodeNextChunk(input)).dropWhile(_._1 == 1).next()
-    (read, Literal(version, typeId, binary(chunks)))
+    (read, Literal(version, binary(chunks)))
 
   def decodeNextChunk(input: Seq[Int])(prefix: Int, chunks: Seq[Int], read: Int): (Int, Seq[Int], Int) =
     val nextPrefix = input(read)
-    val nextGroup = chunks ++ input.slice(read + 1, read + 5)
+    val nextChunk = chunks ++ input.slice(read + 1, read + 5)
     val nextRead = read + 5
-    (nextPrefix, nextGroup, nextRead)
+    (nextPrefix, nextChunk, nextRead)
 
   def decodeOperatorBitLength(version: Long, typeId: Long, input: Seq[Int]): (Int, Packet) =
     val subPackets = binary(input.slice(7, 18)).toInt
@@ -46,24 +46,19 @@ object Day16:
     (read + nextRead, nextPacket :: packets)
 
   def versionSum(packet: Packet): Long = packet match
-    case Literal(version, _, _) => version
+    case Literal(version, _) => version
     case Operator(version, _, packets) => version + packets.map(versionSum).sum
 
   def expressionValue(packet: Packet): Long = packet match
-    case Literal(_, _, value) => value
-    case Operator(_, 0, packets) => packets.map(expressionValue).sum
-    case Operator(_, 1, packets) => packets.map(expressionValue).product
-    case Operator(_, 2, packets) => packets.map(expressionValue).min
-    case Operator(_, 3, packets) => packets.map(expressionValue).max
-    case Operator(_, 5, packets) =>
-      val List(first, second) = packets.map(expressionValue)
-      if first > second then 1L else 0L
-    case Operator(_, 6, packets) =>
-      val List(first, second) = packets.map(expressionValue)
-      if first < second then 1L else 0L
-    case Operator(_, 7, packets) =>
-      val List(first, second) = packets.map(expressionValue)
-      if first == second then 1L else 0L
+    case Literal(_, value) => value
+    case Operator(_, typeId, packets) => (typeId, packets.map(expressionValue)) match
+      case (0, values) => values.sum
+      case (1, values) => values.product
+      case (2, values) => values.min
+      case (3, values) => values.max
+      case (5, List(first, second)) => if first > second then 1L else 0L
+      case (6, List(first, second)) => if first < second then 1L else 0L
+      case (7, List(first, second)) => if first == second then 1L else 0L
 
   def part1(input: Seq[String]): Long = input.map(parse).map(decode).map(_._2).map(versionSum).sum
 
