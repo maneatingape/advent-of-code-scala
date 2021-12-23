c845cc87f8a2f8c99718299824368d3860e3cb26
maneatingape <44142177+maneatingape@users.noreply.github.com>
2021-12-23 22:57:27 +0000

    Linting

--- src/main/scala/AdventOfCode2021/Day23.scala
+++ src/main/scala/AdventOfCode2021/Day23.scala
@@ -4,68 +4,24 @@ object Day23:
   val costPerSpace = Map('A' -> 1, 'B' -> 10, 'C' -> 100, 'D' -> 1000)
   val roomIndex = Map('A' -> 2, 'B' -> 4, 'C' -> 6, 'D' -> 8)
 
-  case class Burrow(roomMax: Int, cost: Int, hallway: Seq[Char], rooms: Map[Char, Seq[Char]])
+  case class Burrow(roomMax: Int, cost: Int, hallway: Seq[Char], rooms: Map[Char, Seq[Char]]):
+    def finished: Boolean = rooms.forall((kind, room) => room.size == roomMax && room.forall(_ == kind))
 
   def small(input: Seq[String]): Burrow =
-    val hallway = Seq.fill(11)('.')
-    val roomA = Seq(input(2)(3), input(3)(3))
-    val roomB = Seq(input(2)(5), input(3)(5))
-    val roomC = Seq(input(2)(7), input(3)(7))
-    val roomD = Seq(input(2)(9), input(3)(9))
-    Burrow(2, 0, hallway, Map('A' -> roomA, 'B' -> roomB, 'C' -> roomC, 'D' -> roomD))
-  end small
+    def room(column: Int) = Seq(input(2)(column), input(3)(column))
+    Burrow(2, 0, Seq.fill(11)('.'), Map('A' -> room(3), 'B' -> room(5), 'C' -> room(7), 'D' -> room(9)))
 
   def large(input: Seq[String]): Burrow =
-    val hallway = Seq.fill(11)('.')
-    val roomA = Seq(input(2)(3), 'D', 'D', input(3)(3))
-    val roomB = Seq(input(2)(5), 'C', 'B', input(3)(5))
-    val roomC = Seq(input(2)(7), 'B', 'A', input(3)(7))
-    val roomD = Seq(input(2)(9), 'A', 'C', input(3)(9))
-    Burrow(4, 0, hallway, Map('A' -> roomA, 'B' -> roomB, 'C' -> roomC, 'D' -> roomD))
-  end large
-
-  def debug(current: Burrow): Unit =
-    def extend(room: Seq[Char]) = (Seq.fill(current.roomMax)('.') ++ room).takeRight(current.roomMax)
-    val extended = current.rooms.view.mapValues(extend)
-    println("Cost: " + current.cost)
-    println(current.hallway.mkString)
-    for i <- 0 until current.roomMax do
-      println("  " + extended('A')(i) + " " + extended('B')(i) + " " + extended('C')(i) + " " + extended('D')(i))
-    println()
-  end debug
+    def room(column: Int, second: Char, third: Char) = Seq(input(2)(column), second, third, input(3)(column))
+    Burrow(4, 0, Seq.fill(11)('.'), Map('A' -> room(3, 'D', 'D'), 'B' -> room(5, 'C', 'B'), 'C' -> room(7, 'B', 'A'), 'D' -> room(9, 'A', 'C')))
 
   def paths(current: Burrow): Seq[Burrow] =
-    val third = current.rooms.flatMap { case (key, room) =>
-      roomToHallway(current, key, room)
-    }
-    val second = current.rooms.flatMap { case (key, room) =>
-      roomToRoom(current, key, room)
-    }
-    val first = current.hallway.zipWithIndex.filter(_._1 != '.').map { (amphipod, index) =>
-      hallwayToRoom(current, amphipod, index)
-    }.flatten
+    val first = current.hallway.zipWithIndex.filter(_._1 != '.').flatMap(hallwayToRoom(current))
+    val second = current.rooms.flatMap(roomToRoom(current))
+    val preferred = first ++ second
+    if preferred.nonEmpty then preferred else current.rooms.flatMap(roomToHallway(current)).toSeq
 
-    val preferred = first.toSeq ++ second
-    if preferred.nonEmpty then preferred else third.toSeq
-  end paths
-
-  def roomToHallway(current: Burrow, key: Char, room: Seq[Char]): Seq[Burrow] =
-    if room.forall(_ == key) then return Seq()
-
-    val index = roomIndex(key)
-    val valid = Seq(0, 1, 3, 5, 7, 9, 10)
-    val left = valid.filter(_ < index).reverse.takeWhile(n => current.hallway(n) == '.')
-    val right = valid.filter(_ > index).takeWhile(n => current.hallway(n) == '.')
-
-    (left.reverse ++ right).map { pos =>
-      val nextH = current.hallway.updated(pos, room.head)
-      val nextR = current.rooms.updated(key, room.tail)
-      val nextC = current.cost + ((pos - index).abs + 1 + current.roomMax - room.size) * costPerSpace(room.head)
-      Burrow(current.roomMax, nextC, nextH, nextR)
-    }
-  end roomToHallway
-
-  def hallwayToRoom(current: Burrow, amphipod: Char, start: Int): Option[Burrow] =
+  def hallwayToRoom(current: Burrow)(amphipod: Char, start: Int): Option[Burrow] =
     val end = roomIndex(amphipod)
     val range = if start < end then ((start + 1) to end) else (end to (start - 1))
 
@@ -77,7 +33,7 @@ object Day23:
     else None
   end hallwayToRoom
 
-  def roomToRoom(current: Burrow, key: Char, room: Seq[Char]): Seq[Burrow] =
+  def roomToRoom(current: Burrow)(key: Char, room: Seq[Char]): Seq[Burrow] =
     if room.forall(_ == key) then return Seq()
 
     val start = roomIndex(key)
@@ -93,24 +49,32 @@ object Day23:
     else Seq()
   end roomToRoom
 
-  def isFinished(burrow: Burrow): Boolean =
-    burrow.rooms.forall { case (key, room) => room.size == burrow.roomMax && room.forall(_ == key) }
+  def roomToHallway(current: Burrow)(key: Char, room: Seq[Char]): Seq[Burrow] =
+    if room.forall(_ == key) then return Seq()
 
-  def shuffle(burrow: Burrow): Int =
-    var total = Int.MaxValue
+    val index = roomIndex(key)
+    val valid = Seq(0, 1, 3, 5, 7, 9, 10)
+    val left = valid.filter(_ < index).reverse.takeWhile(n => current.hallway(n) == '.')
+    val right = valid.filter(_ > index).takeWhile(n => current.hallway(n) == '.')
 
-    def move(current: Burrow): Unit =
-      if isFinished(current) then total = total.min(current.cost)
-      else if current.cost < total then paths(current).foreach(move)
-    end move
+    (left.reverse ++ right).map { pos =>
+      val nextH = current.hallway.updated(pos, room.head)
+      val nextR = current.rooms.updated(key, room.tail)
+      val nextC = current.cost + ((pos - index).abs + 1 + current.roomMax - room.size) * costPerSpace(room.head)
+      Burrow(current.roomMax, nextC, nextH, nextR)
+    }
+  end roomToHallway
 
-    move(burrow)
-    total
-  end shuffle
+  def shuffle(burrow: Burrow): Option[Int] =
+    def move(current: Burrow, energy: Option[Int]): Option[Int] =
+      if current.finished then Some(current.cost)
+      else if energy.exists(_ < current.cost) then None
+      else paths(current).map(move(_, energy)).flatten.minOption
+    move(burrow, None)
 
-  def part1(input: Seq[String]): Int = shuffle(small(input))
+  def part1(input: Seq[String]): Int = shuffle(small(input)).get
 
-  def part2(input: Seq[String]): Int = shuffle(large(input))
+  def part2(input: Seq[String]): Int = shuffle(large(input)).get
 
   def main(args: Array[String]): Unit =
     val data = io.Source.fromResource("AdventOfCode2021/Day23.txt").getLines().toSeq
