20cef344e9ce4182528820a543594c570343f2f3
maneatingape <44142177+maneatingape@users.noreply.github.com>
2022-01-07 01:27:07 +0000

    Cleanup cycle detection

--- src/main/scala/AdventOfCode2018/Day18.scala
+++ src/main/scala/AdventOfCode2018/Day18.scala
@@ -1,7 +1,5 @@
 package AdventOfCode2018
 
-import scala.annotation.tailrec
-
 object Day18:
   val adjacent = for y <- -1 to 1; x <- -1 to 1 if !(x == 0 && y == 0) yield (x, y)
 
@@ -29,24 +27,14 @@ object Day18:
   def part1(input: Seq[String]): Int = Iterator.iterate(parse(input))(step).drop(10).next().score
 
   def part2(input: Seq[String]): Int =
-    @tailrec
-    def helper(grid: Map[Point, Char], index: Int, previous: Seq[Int]): (Int, Seq[Int]) =
-      val score = grid.score
-      val thirdIndex = index + 1
-      val secondIndex = previous.lastIndexOf(score)
-      val firstIndex = previous.lastIndexOf(score, secondIndex - 1)
-
-      if firstIndex != -1 && secondIndex != -1 then
-        val firstSlice = previous.slice(firstIndex, secondIndex)
-        val secondSlice = previous.slice(secondIndex, thirdIndex)
-        if firstSlice.size > 5 && firstSlice == secondSlice then return (firstIndex, firstSlice)
-
-      helper(step(grid), index + 1, previous.appended(score))
-    end helper
+    def helper(grid: Map[Point, Char], index: Int, previous: Seq[Grid]): (Int, Seq[Grid]) =
+      val index = previous.lastIndexOf(grid)
+      if index != -1 then (index, previous.drop(index))
+      else helper(step(grid), index + 1, previous.appended(grid))
 
     val (offset, cycle) = helper(parse(input), 0, Seq())
     val relativeIndex = ((1000000000L - offset) % cycle.size).toInt
-    cycle(relativeIndex)
+    cycle(relativeIndex).score
   end part2
 
   def main(args: Array[String]): Unit =
