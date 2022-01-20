5b45f35a61e14f02339b72555a77f3156953cbf5
maneatingape <44142177+maneatingape@users.noreply.github.com>
2022-01-20 02:21:27 +0000

    Simplify and speedup even further

--- src/main/scala/AdventOfCode2016/Day11.scala
+++ src/main/scala/AdventOfCode2016/Day11.scala
@@ -1,66 +1,51 @@
 package AdventOfCode2016
 
 object Day11:
-  sealed trait RTG
-  case class Chip(name: String) extends RTG
-  case class Generator(name: String) extends RTG
+  val permutations = Seq(Floor(2, 0), Floor(1, 0), Floor(1, 1), Floor(0, 1), Floor(0, 2))
+  val adjacent = Map(0 -> Seq(1), 1 -> Seq(0, 2), 2 -> Seq(1, 3), 3 -> Seq(2))
 
-  case class State(elevator: Int, floors: Seq[Set[RTG]]):
-    def finished: Boolean = elevator == 3 && floors.take(3).forall(_.isEmpty)
-    def valid: Boolean = floors.forall { floor =>
-      val (chips, generators) = floor.partitionMap {
-        case Chip(name) => Left(name)
-        case Generator(name) => Right(name)
-      }
-      generators.isEmpty || chips.subsetOf(generators)
-    }
+  case class Floor(microchips: Int, generators: Int):
+    def empty: Boolean = microchips == 0 && generators == 0
+    def valid: Boolean = (microchips >= 0 && generators >= 0) && (generators == 0 || microchips <= generators)
+    def +(other: Floor): Floor = Floor(microchips + other.microchips, generators + other.generators)
+    def -(other: Floor): Floor = Floor(microchips - other.microchips, generators - other.generators)
+
+  case class State(elevator: Int, floors: Seq[Floor]):
+    def finished: Boolean = elevator == 3 && floors.take(3).forall(_.empty)
+    def valid: Boolean = floors.forall(_.valid)
     def candidates: Seq[State] =
-      val all = for
-        subset <- floors(elevator).subsets(1) ++ floors(elevator).subsets(2)
-        destination <- Set(elevator + 1, elevator - 1).filter(floors.indices.contains)
-      yield State(destination, floors.updated(elevator, floors(elevator) -- subset).updated(destination, floors(destination) ++ subset))
-      all.filter(_.valid).toSeq
-    def equivalent: Seq[(Int, Int)] =
-      floors.map { floor =>
-        floor.foldLeft((0, 0)) { case ((chips, generators), next) =>
-          next match
-            case Chip(_) => (chips + 1, generators)
-            case Generator(_) => (chips, generators + 1)
-        }
-      }
-      .appended((elevator, elevator))
+      for
+        adjust <- permutations
+        destination <- adjacent(elevator)
+      yield State(destination, floors.updated(elevator, floors(elevator) - adjust).updated(destination, floors(destination) + adjust))
 
   def parse(input: Seq[String]): State =
-    val chipRegex = "(\\w+)(?=-compatible)".r
-    val generatorRegex = "(\\w+)(?= generator)".r
-    val floors = input.map(line => (chipRegex.findAllIn(line).map(Chip) ++ generatorRegex.findAllIn(line).map(Generator)).toSet)
-    State(0, floors)
+    val microchip = "microchip".r
+    val generator = "generator".r
+    State(0, input.map(line => Floor(microchip.findAllIn(line).size, generator.findAllIn(line).size)))
 
-  def dijkstra(start: State): Int =
-    val cache = collection.mutable.Map(start.equivalent -> 0)
-    val todo = collection.mutable.PriorityQueue(start -> 0)(Ordering.by((_,c) => -c))
+  def bfs(start: State): Int =
+    val todo = collection.mutable.Queue(start)
+    val cache = collection.mutable.Map(start -> 0)
 
     while todo.nonEmpty do
-      val (current, total) = todo.dequeue()
-      if current.finished then return total
-      current.candidates.foreach { next =>
-        val cost = total + 1
-        val hash = next.equivalent
-        if !cache.contains(hash) || cost < cache(hash) then
-          cache(hash) = cost
-          todo.enqueue(next -> cost)
+      val current = todo.dequeue()
+      val cost = cache(current) + 1
+      current.candidates.filter(_.valid).foreach { next =>
+        if !cache.contains(next) || cost < cache(next) then
+          cache(next) = cost
+          todo.enqueue(next)
       }
 
-    -1
-  end dijkstra
+    cache(cache.keys.filter(_.finished).head)
+  end bfs
 
-  def part1(input: Seq[String]): Int = dijkstra(parse(input))
+  def part1(input: Seq[String]): Int = bfs(parse(input))
 
   def part2(input: Seq[String]): Int =
-    val state = parse(input)
-    val extra = Set(Chip("elerium"), Generator("elerium"), Chip("dilithium"), Generator("dilithium"))
-    val expanded = state.copy(floors = state.floors.updated(0, state.floors(0) ++ extra))
-    dijkstra(expanded)
+    val State(elevator, floors) = parse(input)
+    val expanded = State(elevator, floors.updated(0, floors(0) + Floor(2, 2)))
+    bfs(expanded)
 
   def main(args: Array[String]): Unit =
     val data = io.Source.fromResource("AdventOfCode2016/Day11.txt").getLines().toSeq
