41ca5af2f1b3cce431d5d35bffafecede789767d
maneatingape <44142177+maneatingape@users.noreply.github.com>
2022-01-29 16:47:39 +0000

    Lint

--- src/main/scala/AdventOfCode2019/Day11.scala
+++ src/main/scala/AdventOfCode2019/Day11.scala
@@ -2,37 +2,27 @@ package AdventOfCode2019
 
 import Day09.IntCode
 import Day09.IntCode._
-import Day11.Direction._
 
 object Day11:
-  enum Direction:
-    case Up, Down, Left, Right
-
   case class Point(x: Int, y: Int):
-    def updated(dx: Int, dy: Int): Point = Point(x + dx, y + dy)
+    def +(other: Point): Point = Point(x + other.x, y + other.y)
+    def cw: Point = Point(-y, x)
+    def ccw: Point = Point(y, -x)
 
-  val clockwise = Map(Up -> Right, Right -> Down, Down -> Left, Left -> Up)
-  val antiClockwise = Map(Up -> Left, Left -> Down, Down -> Right, Right -> Up)
-  val deltas = Map(Up -> (0, -1), Down -> (0, 1), Left -> (-1, 0), Right -> (1, 0))
-
-  def paint(intCode: IntCode, direction: Direction, position: Point, panels: Map[Point, Long]): Map[Point, Long] =
+  def paint(intCode: IntCode, direction: Point, position: Point, panels: Map[Point, Long]): Map[Point, Long] =
     val first = intCode.withInput(panels(position)).nextOutput
     first.result match
       case Halted => panels
       case Output(color) =>
         val second = first.nextOutput
         val Output(turn) = second.result
-
-        val nextDirection = if turn == 1 then clockwise(direction) else antiClockwise(direction)
-        val nextPosition = position.updated.tupled(deltas(nextDirection))
-        val nextPanels = panels.updated(position, color)
-
-        paint(second, nextDirection, nextPosition, nextPanels)
+        val nextDirection = if turn == 1 then direction.cw else direction.ccw
+        paint(second, nextDirection, position + nextDirection, panels.updated(position, color))
   end paint
 
-  def part1(memory: Seq[Long]): Long = paint(IntCode(memory), Up, Point(0, 0), Map().withDefaultValue(0L)).size
+  def part1(memory: Seq[Long]): Long = paint(IntCode(memory), Point(0, -1), Point(0, 0), Map().withDefaultValue(0L)).size
 
-  def part2(memory: Seq[Long]): Map[Point, Long] = paint(IntCode(memory), Up, Point(0, 0), Map(Point(0, 0) -> 1L).withDefaultValue(0L))
+  def part2(memory: Seq[Long]): Map[Point, Long] = paint(IntCode(memory), Point(0, -1), Point(0, 0), Map(Point(0, 0) -> 1L).withDefaultValue(0L))
 
   def main(args: Array[String]): Unit =
     val data = io.Source.fromResource("AdventOfCode2019/Day11.txt").mkString.trim.split(",").map(_.toLong).toSeq
