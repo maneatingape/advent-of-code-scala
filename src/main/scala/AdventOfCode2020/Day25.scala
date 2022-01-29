ac9d5edf2477014a053ba45d589705564496de2e
maneatingape <44142177+maneatingape@users.noreply.github.com>
2022-01-29 15:20:12 +0000

    Simplify and use BigInt modPow method

--- src/main/scala/AdventOfCode2020/Day25.scala
+++ src/main/scala/AdventOfCode2020/Day25.scala
@@ -1,16 +1,10 @@
 package AdventOfCode2020
 
 object Day25:
-  def part1(input: Seq[Int]): Long =
-    val (cardPub, doorPub) = (input(0), input(1))
-
-    def transform(subjectNumber: Long) = Iterator.iterate((0, 1L))((n, x) => (n + 1, (x * subjectNumber) % 20201227))
-    def loopSize(pubKey: Int) = transform(7).dropWhile(_._2 != pubKey).next()._1
-
-    val cardLoopSize = loopSize(cardPub)
-    val doorLoopSize = loopSize(doorPub)
-    transform(cardPub).drop(doorLoopSize).next()._2
-  end part1
+  def part1(input: Seq[Int]): BigInt =
+    val Seq(cardPub, doorPub) = input
+    val doorLoopSize = Iterator.iterate(1)(n => (n * 7) % 20201227).indexWhere(_ == doorPub)
+    BigInt(cardPub).modPow(doorLoopSize, 20201227)
 
   def main(args: Array[String]): Unit =
     val data = io.Source.fromResource("AdventOfCode2020/Day25.txt").getLines().map(_.toInt).toSeq
