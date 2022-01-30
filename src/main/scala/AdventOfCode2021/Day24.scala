359cc67510837609e233589e2a6466289127c0cc
maneatingape <44142177+maneatingape@users.noreply.github.com>
2022-01-30 23:27:42 +0000

    Reverse engineering

--- src/main/scala/AdventOfCode2021/Day24.scala
+++ src/main/scala/AdventOfCode2021/Day24.scala
@@ -1,61 +1,40 @@
 package AdventOfCode2021
 
+// Much cleaner solution from reverse engineering the code.
+// The original brute-force solution is preserved in the commit history.
 object Day24:
-  sealed trait Instruction
-  case class Inp(ws: Range) extends Instruction
-  case class Alu(op: (Long, Long) => Long, dest: Int, src: Seq[Long] => Long) extends Instruction
+  sealed trait Round
+  case class Push(offset: Int) extends Round
+  case class Pop(offset: Int) extends Round
 
-  def add(x: Long, y: Long): Long = x + y
-  def mul(x: Long, y: Long): Long = x * y
-  def div(x: Long, y: Long): Long = x / y
-  def mod(x: Long, y: Long): Long = x % y
-  def eql(x: Long, y: Long): Long = if x == y then 1L else 0L
-  def register(i: Int)(input: Seq[Long]): Long = input(i)
-  def constant(i: Int)(input: Seq[Long]): Long = i
+  case class Constraint(index: Int, delta: Int):
+    def min: Int = (1 + delta).max(1)
+    def max: Int = (9 + delta).min(9)
 
-  def parse(input: IndexedSeq[String], smallest: Boolean): IndexedSeq[Instruction] =
-    val indexes = Map("w" -> 0, "x" -> 1, "y" -> 2, "z" -> 3)
-    val execute = Map("add" -> add, "mul" -> mul, "div" -> div, "mod" -> mod, "eql" -> eql)
-    val ws = if smallest then 1 to 9 else 9 to 1 by - 1
-    input.map(_.split(" ")).map {
-      case Array(_, _) => Inp(ws)
-      case Array(op, dest, src) =>
-        Alu(execute(op), indexes(dest), indexes.get(src).map(register).getOrElse(constant(src.toInt)))
-    }
+  def parse(input: Seq[String]): List[Round] = input.grouped(18).toList.map { round =>
+    def helper(n: Int) = round(n).split(" ").last.toInt
+    if helper(4) == 1 then Push(helper(15)) else Pop(helper(5))
+  }
 
-  def execute(instructions: Seq[Instruction]): String =
-    val visited = collection.mutable.Set[(Seq[Long], Int)]()
-    var finished = ""
+  def constraints(rounds: List[Round]): List[Constraint] =
+    def helper(remaining: List[(Round, Int)], stack: List[(Int, Int)], constraints: List[Constraint]): List[Constraint] =
+      remaining match
+        case Nil => constraints.sortBy(_.index)
+        case (Push(firstValue), firstIndex) :: rest =>
+          helper(remaining.tail, (firstValue, firstIndex) :: stack, constraints)
+        case (Pop(secondValue), secondIndex) :: rest =>
+          val (firstValue, firstIndex) = stack.head
+          val delta = firstValue + secondValue
+          helper(remaining.tail, stack.tail, Constraint(firstIndex, -delta) :: Constraint(secondIndex, delta) :: constraints)
 
-    def helper(state: Seq[Long], instructionIndex: Int, path: Seq[Int]): Unit =
-      if finished.nonEmpty then return ()
+    helper(rounds.zipWithIndex, Nil, Nil)
+  end constraints
 
-      if instructionIndex >= instructions.size then
-        if state(3) == 0 then finished = path.mkString
-        return ()
-      end if
+  def part1(input: Seq[String]): String = constraints(parse(input)).map(_.max).mkString
 
-      instructions(instructionIndex) match
-        case Alu(op, dest, src) =>
-          val nextState = state.updated(dest, op(state(dest), src(state)))
-          helper(nextState, instructionIndex + 1, path)
-        case Inp(ws) => ws.foreach { nextW =>
-          val nextState = state.updated(0, nextW.toLong)
-          if !visited.contains(nextState -> instructionIndex) then
-            visited.add(nextState -> instructionIndex)
-            helper(nextState, instructionIndex + 1, path.appended(nextW))
-        }
-    end helper
-
-    helper(Seq(0, 0, 0, 0), 0, Seq())
-    finished
-  end execute
-
-  def part1(input: IndexedSeq[String]): String = execute(parse(input, false))
-
-  def part2(input: IndexedSeq[String]): String = execute(parse(input, true))
+  def part2(input: Seq[String]): String = constraints(parse(input)).map(_.min).mkString
 
   def main(args: Array[String]): Unit =
-    val data = io.Source.fromResource("AdventOfCode2021/Day24.txt").getLines().toIndexedSeq
+    val data = io.Source.fromResource("AdventOfCode2021/Day24.txt").getLines().toSeq
     println(part1(data))
     println(part2(data))
