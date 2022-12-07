70b5d9c8f1d06d9ad6dd289132dede2bf9d8f849
maneatingape <44142177+maneatingape@users.noreply.github.com>
2022-12-07 19:23:00 +0100

    Further cleanup based on structure of input

--- src/main/scala/AdventOfCode2022/Day07.scala
+++ src/main/scala/AdventOfCode2022/Day07.scala
@@ -1,25 +1,18 @@
 package AdventOfCode2022
 
 object Day07:
-  type Path = List[String]
-
   def build(input: Seq[String]): Seq[Int] =
-    val root = List("/")
-    val initial = Map(root -> 0)
-    val (_, sizes) = input.foldLeft((root, initial)) { case ((path, sizes), line) =>
+    val root = List.empty[String]
+    val initial = (root, Map(root -> 0))
+    val (_, sizes) = input.foldLeft(initial) { case ((path, sizes), line) =>
       line match
-        case "$ ls" => (path, update(path, sizes, -sizes(path)))
-        case "$ cd \\" => (List(path.last), sizes)
         case "$ cd .." => (path.tail, sizes)
         case s"$$ cd $name" => (name :: path, sizes.updated(name :: path, 0))
-        case s"dir $name" => (path, sizes)
-        case s"$size $name" => (path, update(path, sizes, size.toInt))
+        case s"$size $name" if size.head.isDigit =>
+          (path, path.tails.foldLeft(sizes)((acc, next) => acc.updated(next, sizes(next) + size.toInt)))
+        case _ => (path, sizes)
     }
     sizes.values.toSeq
-
-  def update(path: Path, sizes: Map[Path, Int], delta: Int): Map[Path, Int] = path match
-    case Nil => sizes
-    case _ :: tail => update(tail, sizes, delta).updated(path, sizes(path) + delta)
 
   def part1(input: Seq[String]): Int =
     val sizes = build(input)
