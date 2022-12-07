2d3e0ee3fb148e623c2d87a503996d185089e323
maneatingape <44142177+maneatingape@users.noreply.github.com>
2022-12-07 08:57:17 +0100

    Immutable cleanup

--- src/main/scala/AdventOfCode2022/Day07.scala
+++ src/main/scala/AdventOfCode2022/Day07.scala
@@ -1,45 +1,31 @@
 package AdventOfCode2022
 
 object Day07:
-  class Dir() {
-    val files = scala.collection.mutable.Map[String, Int]()
-    val dirs = scala.collection.mutable.Map[String, Dir]()
-  }
-
-  def build(input: Seq[String]): Dir = input
-    .foldLeft(List(Dir())) { (path, line) =>
-      line.split(" ") match {
-        case Array("$", "ls") => path
-        case Array("$", "cd", "\\") => List(path.last)
-        case Array("$", "cd", "..") => path.tail
-        case Array("$", "cd", name) => path.head.dirs.getOrElseUpdate(name, Dir()) :: path
-        case Array("dir", name) => path
-        case Array(size, name) =>
-          path.head.files.update(name, size.toInt)
-          path
-      }
+  def build(input: Seq[String]): Seq[Int] =
+    val root = List("/")
+    val initial = Map(root -> 0)
+    val (_, sizes) = input.foldLeft((root, initial)) { case ((path, sizes), line) =>
+      line.split(" ") match
+        case Array("$", "ls") => (path, update(path, sizes, -sizes(path)))
+        case Array("$", "cd", "\\") => (List(path.last), sizes)
+        case Array("$", "cd", "..") => (path.tail, sizes)
+        case Array("$", "cd", name) => (name :: path, sizes.updated(name :: path, 0))
+        case Array("dir", name) => (path, sizes)
+        case Array(size, name) => (path, update(path, sizes, size.toInt))
     }
-    .last
+    sizes.values.toSeq
 
-  def visit(root: Dir): List[Int] =
-    var list = List.empty[Int]
-
-    def helper(current: Dir): Int =
-      val size = current.files.values.sum + current.dirs.values.map(helper).sum
-      list = size :: list
-      size
-
-    helper(root)
-    list
-  end visit
+  def update(path: List[String], sizes: Map[List[String], Int], delta: Int): Map[List[String], Int] = path match
+    case Nil => sizes
+    case head :: tail => update(tail, sizes, delta).updated(path, sizes(path) + delta)
 
   def part1(input: Seq[String]): Int =
-    val sizes = visit(build(input))
+    val sizes = build(input)
     sizes.filter(_ <= 100000).sum
 
   def part2(input: Seq[String]): Int =
-    val sizes = visit(build(input))
-    val needed = 30000000 - (70000000 - sizes.head)
+    val sizes = build(input)
+    val needed = 30000000 - (70000000 - sizes.max)
     sizes.filter(_ >= needed).min
 
   def main(args: Array[String]): Unit =
