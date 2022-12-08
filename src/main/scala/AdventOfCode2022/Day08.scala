c4cac6725d8497d85abac69856c6bd37a707b571
maneatingape <44142177+maneatingape@users.noreply.github.com>
2022-12-08 13:55:57 +0100

    Functional cleanup

--- src/main/scala/AdventOfCode2022/Day08.scala
+++ src/main/scala/AdventOfCode2022/Day08.scala
@@ -1,57 +1,33 @@
 package AdventOfCode2022
 
 object Day08:
-  case class Point(x: Int, y: Int)
+  val orthogonal = Seq(Point(1, 0), Point(-1, 0), Point(0, 1), Point(0, -1))
+
+  case class Point(x: Int, y: Int):
+    def +(other: Point): Point = Point(x + other.x, y + other.y)
+
+  def parse(input: Seq[String]): Map[Point, Int] =
+    val points = for y <- input.indices; x <- input.indices yield Point(x, y) -> input(y)(x).asDigit
+    points.toMap
 
   def part1(input: Seq[String]): Int =
-    val grid = input.map(_.map(_.asDigit))
-    val n = grid.size - 1
-    val visible = collection.mutable.Set[Point]()
+    val grid = parse(input)
 
-    def compare(x: Int, y: Int, tallest: Int): Int =
-      val tree = grid(y)(x)
-      if tree > tallest then visible += Point(x, y)
-      tree.max(tallest)
+    def helper(point: Point, tree: Int)(delta: Point): Boolean =
+      val next = point + delta
+      grid.get(next).forall(_ < tree && helper(next, tree)(delta))
 
-    for i <- 0 to n do
-      var (left, right, top, bottom) = (-1, -1, -1, -1)
-      for j <- 0 to n do
-        left = compare(j, i, left)
-        right = compare(n - j, i, right)
-        top = compare(i, j, top)
-        bottom = compare(i, n - j, bottom)
-
-    visible.size
+    grid.count((k, v) => orthogonal.exists(helper(k, v)))
   end part1
 
-
   def part2(input: Seq[String]): Int =
-    val grid = input.map(_.map(_.asDigit))
-    val n = grid.size - 1
-    var score = 0
+    val grid = parse(input)
 
-    for y <- 0 to n do
-      for x <- 0 to n do
-        val tree = grid(y)(x)
-        def left: Int =
-          for left <- (x - 1) to 0 by -1 do
-            if grid(y)(left) >= tree then return x - left
-          x
-        def right: Int =
-          for right <- (x + 1) to n do
-            if grid(y)(right) >= tree then return right - x
-          n - x
-        def top: Int =
-          for top <- (y - 1) to 0 by -1 do
-            if grid(top)(x) >= tree then return y - top
-          y
-        def bottom: Int =
-          for bottom <- (y + 1) to n do
-            if grid(bottom)(x) >= tree then return bottom - y
-          n - y
-        score = score.max(left * right * bottom * top)
+    def helper(point: Point, tree: Int)(delta: Point): Int =
+      val next = point + delta
+      grid.get(next).map(height => if height < tree then 1 + helper(next, tree)(delta) else 1).getOrElse(0)
 
-    score
+    grid.map((k, v) => orthogonal.map(helper(k, v)).product).max
   end part2
 
   def main(args: Array[String]): Unit =
