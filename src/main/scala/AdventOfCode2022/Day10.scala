5e47ce2b6dadd29803470a92a0c225ac38532a40
maneatingape <44142177+maneatingape@users.noreply.github.com>
2022-12-10 12:38:59 +0100

    Code golfing

--- src/main/scala/AdventOfCode2022/Day10.scala
+++ src/main/scala/AdventOfCode2022/Day10.scala
@@ -1,25 +1,19 @@
 package AdventOfCode2022
 
 object Day10:
-  def parse(input: Seq[String]): Seq[Int] =
-    val (_, signal) = input.foldLeft((1, Seq(-1))) { case ((x, xs), instruction) =>
-      instruction match
-        case "noop" => (x, xs ++ Seq(x))
-        case s"addx $delta" => (x + delta.toInt, xs ++ Seq(x, x))
-    }
-    signal
+  def parse(input: String): Array[Int] =
+    input.split("\\s+").map(_.toIntOption.getOrElse(0)).scanLeft(1)(_ + _)
 
-  def part1(input: Seq[String]): Int =
+  def part1(input: String): Int =
     val signal = parse(input)
-    Seq(20, 60, 100, 140, 180, 220).map(cycle => cycle * signal(cycle)).sum
+    (20 to 220 by 40).map(cycle => cycle * signal(cycle - 1)).sum
 
-  def part2(input: Seq[String]): String =
-    parse(input).tail.grouped(40).map { row =>
-      row.zipWithIndex.map((x, col) => if (x - col).abs <= 1 then '#' else '.')
-    }
+  def part2(input: String): String = parse(input)
+    .init.grouped(40)
+    .map(_.zipWithIndex.map((x, col) => if (x - col).abs <= 1 then '#' else '.'))
     .map(_.mkString).mkString("\n")
 
   def main(args: Array[String]): Unit =
-    val data = io.Source.fromResource("AdventOfCode2022/Day10.txt").getLines().toSeq
+    val data = io.Source.fromResource("AdventOfCode2022/Day10.txt").mkString
     println(part1(data))
     println(part2(data))
--- src/test/scala/AdventOfCode2022/Day10Suite.scala
+++ src/test/scala/AdventOfCode2022/Day10Suite.scala
@@ -3,27 +3,22 @@ package AdventOfCode2022
 import org.scalatest.funsuite.AnyFunSuite
 
 class Day10Suite extends AnyFunSuite:
-  val sample = Seq(
-    "addx 15", "addx -11", "addx 6", "addx -3", "addx 5", "addx -1", "addx -8",
-    "addx 13", "addx 4", "noop", "addx -1", "addx 5", "addx -1", "addx 5",
-    "addx -1", "addx 5", "addx -1", "addx 5", "addx -1", "addx -35", "addx 1",
-    "addx 24", "addx -19", "addx 1", "addx 16", "addx -11", "noop", "noop",
-    "addx 21", "addx -15", "noop", "noop", "addx -3", "addx 9", "addx 1",
-    "addx -3", "addx 8", "addx 1", "addx 5", "noop", "noop", "noop", "noop",
-    "noop", "addx -36", "noop", "addx 1", "addx 7", "noop", "noop", "noop",
-    "addx 2", "addx 6", "noop", "noop", "noop", "noop", "noop", "addx 1",
-    "noop", "noop", "addx 7", "addx 1", "noop", "addx -13", "addx 13", "addx 7",
-    "noop", "addx 1", "addx -33", "noop", "noop", "noop", "addx 2", "noop",
-    "noop", "noop", "addx 8", "noop", "addx -1", "addx 2", "addx 1", "noop",
-    "addx 17", "addx -9", "addx 1", "addx 1", "addx -3", "addx 11", "noop",
-    "noop", "addx 1", "noop", "addx 1", "noop", "noop", "addx -13", "addx -19",
-    "addx 1", "addx 3", "addx 26", "addx -30", "addx 12", "addx -1", "addx 3",
-    "addx 1", "noop", "noop", "noop", "addx -9", "addx 18", "addx 1", "addx 2",
-    "noop", "noop", "addx 9", "noop", "noop", "noop", "addx -1", "addx 2",
-    "addx -37", "addx 1", "addx 3", "noop", "addx 15", "addx -21", "addx 22",
-    "addx -6", "addx 1", "noop", "addx 2", "addx 1", "noop", "addx -10", "noop",
-    "noop", "addx 20", "addx 1", "addx 2", "addx 2", "addx -6", "addx -11",
-    "noop", "noop", "noop")
+  val sample = """
+    addx 15 addx -11 addx 6 addx -3 addx 5 addx -1 addx -8 addx 13 addx 4 noop
+    addx -1 addx 5 addx -1 addx 5 addx -1 addx 5 addx -1 addx 5 addx -1
+    addx -35 addx 1 addx 24 addx -19 addx 1 addx 16 addx -11 noop noop addx 21
+    addx -15 noop noop addx -3 addx 9 addx 1 addx -3 addx 8 addx 1 addx 5 noop
+    noop noop noop noop addx -36 noop addx 1 addx 7 noop noop noop addx 2
+    addx 6 noop noop noop noop noop addx 1 noop noop addx 7 addx 1 noop
+    addx -13 addx 13 addx 7 noop addx 1 addx -33 noop noop noop addx 2 noop
+    noop noop addx 8 noop addx -1 addx 2 addx 1 noop addx 17 addx -9 addx 1
+    addx 1 addx -3 addx 11 noop noop addx 1 noop addx 1 noop noop addx -13
+    addx -19 addx 1 addx 3 addx 26 addx -30 addx 12 addx -1 addx 3 addx 1 noop
+    noop noop addx -9 addx 18 addx 1 addx 2 noop noop addx 9 noop noop noop
+    addx -1 addx 2 addx -37 addx 1 addx 3 noop addx 15 addx -21 addx 22
+    addx -6 addx 1 noop addx 2 addx 1 noop addx -10 noop noop addx 20 addx 1
+    addx 2 addx 2 addx -6 addx -11 noop noop noop
+  """.trim
 
   test("Part 1 should handle sample input correctly") {
     assert(Day10.part1(sample) == 13140)
@@ -31,13 +26,13 @@ class Day10Suite extends AnyFunSuite:
 
   test("Part 2 should handle sample input correctly") {
     val expected = """
-       |##..##..##..##..##..##..##..##..##..##..
-       |###...###...###...###...###...###...###.
-       |####....####....####....####....####....
-       |#####.....#####.....#####.....#####.....
-       |######......######......######......####
-       |#######.......#######.......#######.....
-      """.stripMargin.trim
+      |##..##..##..##..##..##..##..##..##..##..
+      |###...###...###...###...###...###...###.
+      |####....####....####....####....####....
+      |#####.....#####.....#####.....#####.....
+      |######......######......######......####
+      |#######.......#######.......#######.....
+    """.stripMargin.trim
     assert(Day10.part2(sample) == expected)
   }
 
