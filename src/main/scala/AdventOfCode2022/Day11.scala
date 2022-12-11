d8d71cb557b19bd87dbeb162f4e79443456ddece
maneatingape <44142177+maneatingape@users.noreply.github.com>
2022-12-11 09:24:28 +0100

    Tidy

--- src/main/scala/AdventOfCode2022/Day11.scala
+++ src/main/scala/AdventOfCode2022/Day11.scala
@@ -2,23 +2,24 @@ package AdventOfCode2022
 
 object Day11:
   case class Monkey(items: Seq[Long], operation: Long => Long, test: Int, yes: Int, no: Int, count: Long):
+    def compose(g: Long => Long): Monkey = copy(operation = operation.andThen(g))
     def finish: Monkey = copy(items = Seq(), count = count + items.size)
     def accept(extra: Seq[Long]): Monkey = copy(items = items ++ extra)
 
-  def parseNumbers(s: String): Seq[Int] = s.split("\\D+").filter(_.nonEmpty).map(_.toInt).toSeq
+  def parseNumbers(s: String): Seq[Int] = s.split("\\D+").tail.map(_.toInt).toSeq
 
   def parseOperation(s: String): Long => Long = s.split(" ").takeRight(2) match
-    case Array("*", "old") => o => o * o
-    case Array("*", x) => _ * x.toLong
-    case Array("+", x) => _ + x.toLong
+    case Array("*", "old") => x => x * x
+    case Array("*", y) => x => x * y.toLong
+    case Array("+", y) => x => x + y.toLong
 
-  def parse(input: String): Seq[Monkey] =
-    input.split("\n\n").map(_.split("\n")).toSeq.map { lines =>
+  def parse(input: Seq[String]): Seq[Monkey] =
+    input.grouped(7).toSeq.map { lines =>
       val items = parseNumbers(lines(1)).map(_.toLong)
       val operation = parseOperation(lines(2))
-      val test = parseNumbers(lines(3)).last
-      val yes = parseNumbers(lines(4)).last
-      val no = parseNumbers(lines(5)).last
+      val test = parseNumbers(lines(3)).head
+      val yes = parseNumbers(lines(4)).head
+      val no = parseNumbers(lines(5)).head
       Monkey(items, operation, test, yes, no, 0)
     }
 
@@ -35,18 +36,18 @@ object Day11:
   def play(monkeys: Seq[Monkey], rounds: Int): Long =
     Iterator.iterate(monkeys)(step).drop(rounds).next().map(_.count).sorted.takeRight(2).product
 
-  def part1(input: String): Long =
+  def part1(input: Seq[String]): Long =
     val monkeys = parse(input)
-    val adjusted = monkeys.map(monkey => monkey.copy(operation = monkey.operation(_) / 3))
+    val adjusted = monkeys.map(_.compose(_ / 3))
     play(adjusted, 20)
 
-  def part2(input: String): Long =
+  def part2(input: Seq[String]): Long =
     val monkeys = parse(input)
-    val modulus = monkeys.map(_.test).product
-    val adjusted = monkeys.map(monkey => monkey.copy(operation = monkey.operation(_) % modulus))
+    val product = monkeys.map(_.test).product
+    val adjusted = monkeys.map(_.compose(_ % product))
     play(adjusted, 10000)
 
   def main(args: Array[String]): Unit =
-    val data = io.Source.fromResource("AdventOfCode2022/Day11.txt").mkString
+    val data = io.Source.fromResource("AdventOfCode2022/Day11.txt").getLines().toSeq
     println(part1(data))
     println(part2(data))
--- src/test/scala/AdventOfCode2022/Day11Suite.scala
+++ src/test/scala/AdventOfCode2022/Day11Suite.scala
@@ -3,35 +3,34 @@ package AdventOfCode2022
 import org.scalatest.funsuite.AnyFunSuite
 
 class Day11Suite extends AnyFunSuite:
-  val sample = """
-    Monkey 0:
-      Starting items: 79, 98
-      Operation: new = old * 19
-      Test: divisible by 23
-        If true: throw to monkey 2
-        If false: throw to monkey 3
-
-    Monkey 1:
-      Starting items: 54, 65, 75, 74
-      Operation: new = old + 6
-      Test: divisible by 19
-        If true: throw to monkey 2
-        If false: throw to monkey 0
-
-    Monkey 2:
-      Starting items: 79, 60, 97
-      Operation: new = old * old
-      Test: divisible by 13
-        If true: throw to monkey 1
-        If false: throw to monkey 3
-
-    Monkey 3:
-      Starting items: 74
-      Operation: new = old + 3
-      Test: divisible by 17
-        If true: throw to monkey 0
-        If false: throw to monkey 1
-  """.trim
+  val sample = Seq(
+    "Monkey 0:",
+    "  Starting items: 79, 98",
+    "  Operation: new = old * 19",
+    "  Test: divisible by 23",
+    "    If true: throw to monkey 2",
+    "    If false: throw to monkey 3",
+    "",
+    "Monkey 1:",
+    "  Starting items: 54, 65, 75, 74",
+    "  Operation: new = old + 6",
+    "  Test: divisible by 19",
+    "    If true: throw to monkey 2",
+    "    If false: throw to monkey 0",
+    "",
+    "Monkey 2:",
+    "  Starting items: 79, 60, 97",
+    "  Operation: new = old * old",
+    "  Test: divisible by 13",
+    "    If true: throw to monkey 1",
+    "    If false: throw to monkey 3",
+    "",
+    "Monkey 3:",
+    "  Starting items: 74",
+    "  Operation: new = old + 3",
+    "  Test: divisible by 17",
+    "    If true: throw to monkey 0",
+    "    If false: throw to monkey 1")
 
   test("Part 1 should handle sample input correctly") {
     assert(Day11.part1(sample) == 10605L)
