cbf362700369165a733b046caba57ccbdc883ae5
maneatingape <44142177+maneatingape@users.noreply.github.com>
2022-12-12 08:16:16 +0100

    Tidy

--- src/main/scala/AdventOfCode2022/Day12.scala
+++ src/main/scala/AdventOfCode2022/Day12.scala
@@ -1,30 +1,33 @@
 package AdventOfCode2022
 
 object Day12:
-  val orthogonal = Seq(Point(1, 0), Point(-1, 0), Point(0, 1), Point(0, -1))
+  val orthogonal = Seq((1, 0), (-1, 0), (0, 1), (0, -1))
 
   case class Point(x: Int, y: Int):
-    def delta(other: Point): Point = Point(x + other.x, y + other.y)
+    def delta(dx: Int, dy: Int): Point = Point(x + dx, y + dy)
 
-  def parse(input: Seq[String]): Map[Point, Char] =
-    val points = for y <- input.indices; x <- input.head.indices yield Point(x, y) -> input(y)(x)
-    points.toMap
-
-  def bfs(grid: Map[Point, Char], end: Char): Int =
-    val (start, _) = grid.find((k, v) => v == 'E').get
-    val todo = collection.mutable.Queue(start)
-    val cost = collection.mutable.Map(start -> 0)
-
+  type Grid = Map[Point, Char]
+  extension (grid: Grid)
+    def neighbours(point: Point): Seq[Point] = orthogonal.map(point.delta).filter(grid.contains)
     def height(point: Point): Char = grid(point) match
       case 'S' => 'a'
       case 'E' => 'z'
       case c => c
 
+  def parse(input: Seq[String]): Grid =
+    val points = for y <- input.indices; x <- input.head.indices yield Point(x, y) -> input(y)(x)
+    points.toMap
+
+  def bfs(grid: Grid, end: Char): Int =
+    val start = grid.map(_.swap)('E')
+    val todo = collection.mutable.Queue(start)
+    val cost = collection.mutable.Map(start -> 0)
+
     while todo.nonEmpty do
       val point = todo.dequeue()
       if grid(point) == end then return cost(point)
-      orthogonal.map(point.delta).filter(grid.contains).foreach { next =>
-        if height(point) - height(next) <= 1 && !cost.contains(next) then
+      grid.neighbours(point).filterNot(cost.contains).foreach { next =>
+        if grid.height(point) - grid.height(next) <= 1 then
           todo.enqueue(next)
           cost(next) = cost(point) + 1
       }
