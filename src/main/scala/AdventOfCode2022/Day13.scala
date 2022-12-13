762c8feb974413d0e50b77160e95aa64e574f6bc
maneatingape <44142177+maneatingape@users.noreply.github.com>
2022-12-13 08:27:28 +0100

    Further tidy

--- src/main/scala/AdventOfCode2022/Day13.scala
+++ src/main/scala/AdventOfCode2022/Day13.scala
@@ -1,26 +1,23 @@
 package AdventOfCode2022
 
 object Day13:
-  def compare(left: String, right: String): Boolean =
-    if left.head == right.head then compare(left.tail, right.tail)
-    else (left.head, right.head) match
-      case (']', _) => true
-      case (_, ']') => false
-      case ('[', b) => compare(left.tail, b + "]" + right.tail)
-      case (a, '[') => compare(a + "]" + left.tail, right.tail)
-      case (a, b) => a < b
+  def simplify(input: Seq[String]): Seq[String] = input.filter(_.nonEmpty).map(_.replace("10", "A"))
 
-  def parse(input: Seq[String]): Seq[String] =
-    input.filter(_.nonEmpty).map(_.replace("10", "A"))
+  def compare(left: String, right: String): Boolean = (left.head, right.head) match
+    case (a, b) if a == b => compare(left.tail, right.tail)
+    case (']', _) => true
+    case (_, ']') => false
+    case ('[', b) => compare(left.tail, b + "]" + right.tail)
+    case (a, '[') => compare(a + "]" + left.tail, right.tail)
+    case (a, b) => a < b
 
   def part1(input: Seq[String]): Int =
-    parse(input).grouped(2).zipWithIndex
-      .collect { case (Seq(left, right), index) if compare(left, right) => index + 1 }
-      .sum
+    val pairs = simplify(input).grouped(2).zipWithIndex
+    pairs.collect { case (Seq(left, right), index) if compare(left, right) => index + 1 }.sum
 
   def part2(input: Seq[String]): Int =
     val dividers = Seq("[[2]]", "[[6]]")
-    val sorted = (parse(input) ++ dividers).sortWith(compare)
+    val sorted = (simplify(input) ++ dividers).sortWith(compare)
     dividers.map(sorted.indexOf(_) + 1).product
 
   def main(args: Array[String]): Unit =
