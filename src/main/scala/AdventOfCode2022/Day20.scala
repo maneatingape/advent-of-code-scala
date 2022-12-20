e9894ae1ed274fc693c928f41c9dfcbf051452ff
maneatingape <44142177+maneatingape@users.noreply.github.com>
2022-12-20 23:08:56 +0100

    Simplify

--- src/main/scala/AdventOfCode2022/Day20.scala
+++ src/main/scala/AdventOfCode2022/Day20.scala
@@ -1,40 +1,21 @@
 package AdventOfCode2022
 
 object Day20:
-  case class Node(value: Long, var prev: Node, var next: Node)
-
-  def parse(input: Seq[Int], key: Long): Seq[Node] =
-    val nodes = input.map(n => Node(n * key, null, null))
-    nodes.zipWithIndex.foreach { (node, i) =>
-      node.prev = nodes((i - 1 + nodes.size) % nodes.size)
-      node.next = nodes((i + 1) % nodes.size)
-    }
-    nodes
-
-  def mix(nodes: Seq[Node]): Unit =
-    for node <- nodes do
-      val remainder = (node.value % (nodes.size - 1)).toInt
-      val move = if remainder >= 0 then remainder else remainder + nodes.size - 1
-      for _ <- 1 to move do
-        val (a, b, c, d) = (node.prev, node, node.next, node.next.next)
-        a.next = c
-        b.prev = c
-        b.next = d
-        c.prev = a
-        c.next = b
-        d.prev = b
+  def decrypt(input: Seq[Int], key: Long, rounds: Int): Long =
+    val mixed = collection.mutable.ArrayBuffer.from(input.map(_ * key).zipWithIndex)
+    for _ <- 1 to rounds do
+      for index <- input.indices do
+        val from = mixed.indexWhere(_._2 == index)
+        val pair @ (number, _) = mixed.remove(from)
+        val remainder = (number % mixed.size).toInt
+        val to = (from + remainder + mixed.size) % mixed.size
+        mixed.insert(to, pair)
       end for
     end for
+    val start = mixed.indexWhere(_._1 == 0)
+    (1 to 3).map(offset => mixed((start + 1000 * offset) % mixed.size)._1).sum
 
-  def skip(start: Node): Node = Iterator.iterate(start)(_.next).drop(1000).next()
-
-  def decrypt(input: Seq[Int], key: Long, rounds: Int): Long =
-    val nodes = parse(input, key)
-    for _ <- 1 to rounds do mix(nodes)
-    val start = nodes.find(_.value == 0).get
-    Iterator.iterate(start)(skip).drop(1).take(3).map(_.value).sum
-
-  def part1(input: Seq[Int]): Long = decrypt(input, 1, 1)
+  def part1(input: Seq[Int]): Long = decrypt(input, 1L, 1)
 
   def part2(input: Seq[Int]): Long = decrypt(input, 811589153L, 10)
 
--- src/test/scala/AdventOfCode2022/Day20Suite.scala
+++ src/test/scala/AdventOfCode2022/Day20Suite.scala
@@ -3,7 +3,7 @@ package AdventOfCode2022
 import org.scalatest.funsuite.AnyFunSuite
 
 class Day20Suite extends AnyFunSuite:
-  val sample = Seq(1, 2 , -3, 3 , -2, 0, 4)
+  val sample = Seq(1, 2, -3, 3, -2, 0, 4)
 
   test("Part 1 should handle sample input correctly") {
     assert(Day20.part1(sample) == 3)
