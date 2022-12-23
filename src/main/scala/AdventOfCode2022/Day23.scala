8c9020b11e6fd6045b8c47c3dc4aeeb3e1f4e721
maneatingape <44142177+maneatingape@users.noreply.github.com>
2022-12-23 08:12:26 +0100

    Tidy

--- src/main/scala/AdventOfCode2022/Day23.scala
+++ src/main/scala/AdventOfCode2022/Day23.scala
@@ -2,8 +2,8 @@ package AdventOfCode2022
 
 object Day23:
   val adjacent = Seq(Point(-1, -1), Point(0, -1), Point(1, -1), Point(-1, 0), Point(1, 0), Point(-1, 1), Point(0, 1), Point(1, 1))
-  val (north, south, west, east) = (Point(0, -1), Point(0, 1), Point(-1, 0), Point(1, 0))
-  val order = Seq(north, south, west, east)
+  val order = Seq(Point(0, -1), Point(0, 1), Point(-1, 0), Point(1, 0))
+  val Seq(north, south, west, east) = order
 
   case class Point(x: Int, y: Int):
     def +(other: Point): Point = Point(x + other.x, y + other.y)
@@ -19,20 +19,19 @@ object Day23:
 
     val proposals = elves.map(elf => elf -> propose(elves, moves, elf))
     val occurrences = proposals.toSeq.flatMap(_._2).groupMapReduce(identity)(_ => 1)(_ + _)
-    val valid = occurrences.filter((_, total) => total == 1).keySet
     val next = proposals.map { (elf, proposal) =>
-      proposal.map(move => if valid.contains(move) then move else elf).getOrElse(elf)
+      proposal.map(move => if occurrences(move) == 1 then move else elf).getOrElse(elf)
     }
 
     State(next, moves.tail :+ moves.head, elves == next)
   end step
 
-  def propose(elves: Set[Point], order: Seq[Point], elf: Point): Option[Point] =
+  def propose(elves: Set[Point], moves: Seq[Point], elf: Point): Option[Point] =
     val checks = adjacent.map(_ + elf).map(elves.contains)
     val Seq(nw, n, ne, w, e, sw, s, se) = checks
 
     if checks.exists(identity) then
-      order.find {
+      moves.find {
         case `north` => !(nw || n || ne)
         case `south` => !(sw || s || se)
         case `west` => !(nw || w || sw)
@@ -46,10 +45,8 @@ object Day23:
     val start = State(parse(input), order, false)
     val elves = Iterator.iterate(start)(step).drop(10).next.elves
 
-    val minX = elves.map(_.x).min
-    val maxX = elves.map(_.x).max
-    val minY = elves.map(_.y).min
-    val maxY = elves.map(_.y).max
+    val (minX, maxX) = (elves.map(_.x).min, elves.map(_.x).max)
+    val (minY, maxY) = (elves.map(_.y).min, elves.map(_.y).max)
 
     val points = for x <- minX to maxX; y <- minY to maxY yield Point(x, y)
     points.filterNot(elves.contains).size
